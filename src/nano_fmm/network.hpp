cc8678af74ac1ef758080118a63029039708b895
TANG ZHIXIONG <zhixiong.tang@momenta.ai>
2023-08-18 19:14:08 +0800

    init types

---
+++ src/nano_fmm/network.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace nano_fmm {
+    struct Network {
+        Network(bool is_wgs84 = true) : is_wgs84_(is_wgs84) {}
+
+
+
+
+        private:
+        const bool is_wgs84_{true};
+    };
+}
