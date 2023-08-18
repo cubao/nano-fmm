d2876bb3a39bea6f402220862fca5b3fe5f1beea
TANG ZHIXIONG <zhixiong.tang@momenta.ai>
2023-08-18 19:26:08 +0800

    update

--- src/nano_fmm/types.hpp
+++ src/nano_fmm/types.hpp
@@ -1,3 +1,18 @@
 #pragma once
 
-// eigen types
+// https://github.com/microsoft/vscode-cpptools/issues/9692
+#if __INTELLISENSE__
+#undef __ARM_NEON
+#undef __ARM_NEON__
+#endif
+
+#include <Eigen/Core>
+
+namespace nano_fmm {
+// Nx3 vectors (row major, just like numpy ndarray)
+using RowVectors = Eigen::Matrix<double, Eigen::Dynamic, 3, Eigen::RowMajor>;
+using RowVectorsNx3 = RowVectors;
+// without-z
+using RowVectorsNx2 = Eigen::Matrix<double, Eigen::Dynamic, 2, Eigen::RowMajor>;
+}
+
