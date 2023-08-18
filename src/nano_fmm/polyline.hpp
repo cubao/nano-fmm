771f513911ea53c4fc00431d1c72c26a040b8a8c
TANG ZHIXIONG <zhixiong.tang@momenta.ai>
2023-08-18 19:29:46 +0800

    add polyline

---
+++ src/nano_fmm/polyline.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "nano_fmm/types.hpp"
+
+namespace nano_fmm {
+// https://github.com/cubao/pybind11-rdp/blob/master/src/main.cpp
+struct LineSegment
+{
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+    const Eigen::Vector3d A, B, AB;
+    const double len2, inv_len2;
+    LineSegment(const Eigen::Vector3d &a, const Eigen::Vector3d &b)
+        : A(a), B(b), AB(b - a), //
+          len2((b - a).squaredNorm()), inv_len2(1.0 / len2)
+    {
+    }
+    double distance2(const Eigen::Vector3d &P) const
+    {
+        double dot = (P - A).dot(AB);
+        if (dot <= 0) {
+            return (P - A).squaredNorm();
+        } else if (dot >= len2) {
+            return (P - B).squaredNorm();
+        }
+        // P' = A + dot/length * normed(AB)
+        //    = A + dot * AB / (length^2)
+        return (A + (dot * inv_len2 * AB) - P).squaredNorm();
+    }
+    double distance(const Eigen::Vector3d &P) const
+    {
+        return std::sqrt(distance2(P));
+    }
+};
+    
+}
